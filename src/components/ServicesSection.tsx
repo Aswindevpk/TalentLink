848faf840ac1ded868d8cb4f6e16e48f57e4065a
Aswindevpk <aswindevpk014@gmail.com>
2025-04-11 10:43:50 +0530

    setup ci/cd

---
+++ src/components/ServicesSection.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledServicesSection = styled.div`
+
+`;
+
+
+function ServicesSection() {
+    return (
+        <StyledServicesSection>ServicesSection</StyledServicesSection>
+    )
+}
+
+export default ServicesSection
