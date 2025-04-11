0b71dc9d08c1079d721330640e0eb9ef8f24b7ef
Aswindevpk <aswindevpk014@gmail.com>
2025-04-11 14:50:31 +0530

    fix: set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()], 
-  
-})
+export default defineConfig(({ command }) => ({
+  plugins: [react()],
+  base: command === 'build' ? '/TalentLink/' : '/', // 👈 Only apply base path in build
+}));
